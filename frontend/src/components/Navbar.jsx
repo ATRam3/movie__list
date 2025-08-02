54756525e5f00534d1e6cfbb5cf02fa4562234f2
amen <amenteshomereg@gmail.com>
2025-08-02 02:23:49 -0700

    fixed the error on the desplaying the favorites and now i am also working on the movie detail icon favorite heart icon to render to the favorite page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,60 +1,13 @@
 import { Link } from "react-router-dom"
 import "../css/Navbar.css"
-import { useState, useEffect } from "react";
-import { searchMovies, getPopularMovies } from "../services/api";
-import { FaSearch } from "react-icons/fa";
 
 function Navbar(){
-
-    const [searchQuery, setSearchQuery] = useState("");
-    const [movies, setMovies] = useState([]);
-    const [error, setError] = useState(null);
-    const [loading, setLoading] = useState(true);
-
- 
-
-  const handleSearch = async (e) => {
-        e.preventDefault();
-
-        if(!searchQuery.trim()) return
-        if(loading) return
-
-        setLoading(true);
-
-        try{
-        const searchResults = await searchMovies(searchQuery);
-        setMovies(searchResults);
-        setError(null);
-        } catch(err) {
-        console.log(err)
-        setError("Failed to search movies...");
-        } finally {
-        setLoading(false);
-        }
-
-    };
-
 
     return(
         <nav className="navbar">
             <div className="navbar-brand">
                 <Link to="/">Movie App</Link>
             </div>
-
-            <form onSubmit={handleSearch} className="search-form">
-                <div className="search-container">
-                <input
-                    type="text"
-                    placeholder="Search for movies..."
-                    className="search-input"
-                    value={searchQuery}
-                    onChange={(e) => setSearchQuery(e.target.value)}
-                />
-                <button type="submit" className="search-button">
-                    <FaSearch  className="search-icon"/>
-                </button>
-                </div>
-            </form>
 
 
             <div className="navbar-link">
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,13 +3,13 @@ import MovieCard from "../components/MovieCard";
 import { useState, useEffect } from "react";
 import { searchMovies, getPopularMovies } from "../services/api";
 import "../css/Home.css";
-import { MdOutlineSearch } from "react-icons"
+import { FaSearch } from 'react-icons/fa';
 
 function Home() {
-  
-  const [movies, setMovies] = useState([]);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(true);
+    const [searchQuery, setSearchQuery] = useState("");
+    const [movies, setMovies] = useState([]);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const loadPopularMovies = async () => {
@@ -26,6 +26,28 @@ function Home() {
 
     loadPopularMovies();
   }, []);
+
+
+  const handleSearch = async (e) => {
+        e.preventDefault();
+
+        if(!searchQuery.trim()) return
+        if(loading) return
+
+        setLoading(true);
+
+        try{
+        const searchResults = await searchMovies(searchQuery);
+        setMovies(searchResults);
+        setError(null);
+        } catch(err) {
+        console.log(err)
+        setError("Failed to search movies...");
+        } finally {
+        setLoading(false);
+        }
+
+    };
 
    /* const movies = [
     {id: 1, title: "terminator", release__date: "2020"},
@@ -46,7 +68,6 @@ function Home() {
           />
           <button type="submit" className="search-button">
             <FaSearch  className="search-icon"/>
-            <MdOutlineSearch />
           </button>
         </div>
       </form>
